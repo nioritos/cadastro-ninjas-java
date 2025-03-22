d9396fdb3bf1d7fdd3712725b28355518df26460
Junior Guimarães <junioradr2008@gmail.com>
2025-03-22 12:25:55 -0300

    feat lombok

--- src/main/java/dev/nioritos/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/nioritos/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,16 +1,17 @@
 package dev.nioritos.CadastroDeNinjas.Ninjas;
-
 import dev.nioritos.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
-
-//just other comment to amend on git
 @Entity
 //ele transforma a classe em uma interface do banco de dados
 @Table(name = "tb_cadastro")
 //nomeia a tabela para o banco de dados
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,33 +24,5 @@ public class NinjaModel {
     private MissoesModel missoes;
 
 
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
