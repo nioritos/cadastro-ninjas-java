3830b78d83ea281eae46af8adb280e1425e9dd11
Junior Guimarães <junioradr2008@gmail.com>
2025-03-25 23:38:12 -0300

    feat: add endpoints

--- src/main/java/dev/nioritos/CadastroDeNinjas/Ninjas/Controller.java
+++ src/main/java/dev/nioritos/CadastroDeNinjas/Ninjas/Controller.java
@@ -1,6 +1,8 @@
 package dev.nioritos.CadastroDeNinjas.Ninjas;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +12,25 @@ public class Controller {
     @GetMapping("/home")
     public String boasVindas() {
         return "first msg on route";
+    };
+    
+    @GetMapping("/ninjasPerId")
+    public String mostrarNinjaPorID() {
+        return "show ninja per id";
+    };
+
+    @GetMapping("/todos")
+    public String mostrarTodosNinjas() {
+        return "show all ninjas";
+    };
+
+    @PutMapping("/alterarNinja")
+    public String alterarNinja() {
+        return "alter ninja";
+    };
+
+    @DeleteMapping("deletarNinja")
+    public String deletarNinja() {
+        return "delete ninja";
     };
 }
