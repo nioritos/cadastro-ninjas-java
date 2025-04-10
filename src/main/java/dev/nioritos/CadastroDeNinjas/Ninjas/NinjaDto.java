ee3369e8afe14ad3da00f515d646cc065ba0260e
Junior Guimarães <junioradr2008@gmail.com>
2025-04-10 20:49:26 -0300

    feat: create dto with values

---
+++ src/main/java/dev/nioritos/CadastroDeNinjas/Ninjas/NinjaDto.java
@@ -0,0 +1,17 @@
+package dev.nioritos.CadastroDeNinjas.Ninjas;
+
+import dev.nioritos.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class NinjaDto {
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String imgUrl;
+    private MissoesModel missoes;
+}
